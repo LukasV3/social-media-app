5e85d3ecc8ae14b569d761ab5784ec69664582e0
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-01-09 18:01:47 +0000

    users can post text content and have their posts rendered on their feed

--- public/index.js
+++ public/index.js
@@ -1,9 +1,11 @@
 import "@babel/polyfill";
 import { login, signup } from "./login";
+import { createPost } from "./post";
 
 // DOM ELEMENTS
 const loginFormEl = document.querySelector(".login-form");
 const signupFormEl = document.querySelector(".signup-form");
+const createPostEl = document.querySelector(".create-post-div");
 
 // DELEGATION
 
@@ -22,5 +24,16 @@ if (signupFormEl) {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     signup(username, password);
+  });
+}
+
+if (createPostEl) {
+  const shareBtn = document.querySelector(".share-post");
+  shareBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const postContent = document.getElementById("message-content").value;
+    const username = document.getElementById("currentUserUsername").textContent.slice(1);
+
+    createPost(username, postContent);
   });
 }
---
+++ public/post.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const createPost = async (username, content) => {
+  console.log(username, content);
+
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "/api/v1/users/post",
+      data: {
+        username,
+        content,
+      },
+    });
+    // axios will put data object keys on req.body
+    if (res.data.status === "success") {
+      //   showAlert("success", "You have successfully created an account!");
+
+      location.assign("/feed");
+    }
+  } catch (err) {}
+};
