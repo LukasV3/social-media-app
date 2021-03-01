c1af70144d4b1a380985ee5e076b3c0b5fc7da11
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-03-01 11:22:23 +0000

    wired up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
 import "./styles/index.scss";
 
-function App() {
-  return <div>app</div>;
-}
+import history from "./history";
+import Login from "./components/login/Login";
+
+const App = () => {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact component={Login} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
