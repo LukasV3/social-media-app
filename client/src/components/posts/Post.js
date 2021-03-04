5f573fcd2757751659585142e52ce92190681196
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-03-04 12:54:42 +0000

    implementing post createdAt formatting

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -1,14 +1,31 @@
 import React from "react";
 import "./styles.scss";
+import dateFormat from "dateformat";
 
 const Post = ({ post }) => {
+  // const dateOptions = { day: "numeric", month: "long" };
+  // console.log(new Date(post.createdAt).get);
+
+  const renderCreatedAt = (createdAt) => {
+    const date = new Date(createdAt);
+    const day = dateFormat(date, "DDDD");
+    if (day === "Today" || day === "Yesterday") {
+      const newDate = `${day} at ${dateFormat(date, "HH:MM TT")}`;
+      console.log(newDate);
+    } else {
+      const newDate = `${dateFormat(date, "d:MM TT")}`;
+    }
+  };
+
   return (
     <div className="post">
       <div className="post__user-info">
         <div className="post__user-info--img"></div>
         <p className="post__user-info--name">{post.username}</p>
         <p className="post__user-info--date">
-          {new Date(post.createdAt).toLocaleString().slice(0, -3)}
+          {renderCreatedAt(post.createdAt)}
+          {/* {console.log(post)} */}
+          {/* {new Date(post.createdAt).toLocaleString("en-US", dateOptions)} */}
         </p>
       </div>
       <p className="post__content">{post.content}</p>
